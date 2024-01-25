3a6a28d9f1ef73714ce483556efeb32e618c9743
Vc <vcvckw@gmail.com>
2024-01-26 00:09:24 +0800

    add Pool

---
+++ v3/Pool/Pool.cpp
@@ -0,0 +1,88 @@
+//
+// Created by Vc on 1/16/24.
+//
+
+#include "Pool.h"
+
+#include <vector>
+#include <iostream>
+
+Pool::Pool(double units, double fall_ratio, double profit_lock, double profit_rtatio, double re_share_ratio, double max_reward,
+            double  spot_price, double pool_dbr, double pool_layer, bool pool_status,
+           const std::vector<double> &layer_input, const std::vector<double> layer_price) :
+     m_units(units), m_fall_ratio(fall_ratio), m_profit_lock(profit_lock), m_profit_rtatio(profit_rtatio), m_re_share_ratio(re_share_ratio), m_max_reward(max_reward),
+     m_spot_price(spot_price), m_pool_dbr(pool_dbr), m_pool_layer(pool_layer), m_pool_status(pool_status), pool_layer_input(layer_input), pool_layer_price(layer_price)
+{
+//    std::cout << " m_units = " << m_units << "\n m_fall_ratio = " << m_fall_ratio << "\n m_profit_lock = " << m_profit_lock << "\n m_profit_rtatio = " << m_profit_rtatio <<
+//    "\n m_re_share_ratio = " << m_re_share_ratio << "\n m_max_reward = " << m_max_reward << "\n m_spot_price = " << m_spot_price << "\n m_pool_dbr = " << m_pool_dbr << "\n m_pool_layer = " << m_pool_layer << "\n m_pool_status = " << m_pool_status << std::endl;
+
+    for (int i = 0; i < pool_layer_input.size(); i++) {
+        m_pool_input_count += pool_layer_input[i];
+        m_pool_tvl += pool_layer_input[i] * pool_layer_price[i] * units;
+        pool_layer_tvl.push_back(std::make_pair(i, pool_layer_input[i] * pool_layer_price[i] * m_units)); //Layer_tvl
+    }
+
+
+    for (auto it = pool_layer_tvl.begin(); it != pool_layer_tvl.end(); ++it) //Show Layer_tvl
+    {
+        std::cout << "Layer: " << it->first << ", TVL: " << it->second << std::endl;
+    }
+    std::cout << "ALL_POOL_TVL: " <<  m_pool_tvl << std::endl;
+
+//    std::cout << "m_pool_input_count: " << m_pool_input_count << std::endl;
+    //平均价
+    for (auto it = pool_layer_tvl.begin(); it != pool_layer_tvl.end(); ++it) //Show Layer_tvl
+    {
+        m_avg_price = m_pool_tvl / m_pool_input_count / m_units;
+    }
+    std::cout << "Avg_price: " << m_avg_price << std::endl;
+
+    //止盈价
+    m_profit_price = m_avg_price + m_avg_price * m_profit_lock;
+    std::cout << "Profit_price: " << m_profit_price << std::endl;
+
+}
+
+void Pool::ca_layer_dbr() //downtvl/uptvl/dbr
+{
+    for (auto it = pool_layer_tvl.begin(); it != pool_layer_tvl.end(); ++it) {
+        m_downtvl += it->second * (it->first + 1);
+    }
+
+    double t_layer_dbr = 0;
+    for (auto it = pool_layer_tvl.begin(); it != pool_layer_tvl.end(); ++it){
+        m_uptvl = it->second * (it->first + 1);
+        t_layer_dbr = m_pool_dbr * m_uptvl / m_downtvl;
+        std::cout << "layer" << it->first << "_dbr = "  << t_layer_dbr << std::endl;
+        layer_dbr.push_back(t_layer_dbr);
+    }
+}
+
+//根据池子筹码，参数计算奖池奖金
+void Pool::ca_pool_esreward()
+{
+    double es_r1 = m_profit_price - m_avg_price;
+    double es_r2 = es_r1 * (1 - FEE::ECO_FEE);
+    double es_rewards = es_r2 / m_spot_price * m_pool_input_count * m_profit_lock * m_profit_rtatio * m_units;
+    std::cout << "Estimated Rewards: " << es_rewards << std::endl;
+}
+
+void Pool::addUser(User* user){
+    m_users.push_back(user);
+}
+
+void Pool::distribute() {
+    // 遍历每个用户，执行策略
+    for (auto user : m_users) {
+        user->executeStrategy(this);
+    }
+//    // 输出每个用户的奖励
+//    for (auto user : users) {
+//        cout << user->getName() << " 投入了 " << user->getCoins() << " 个硬币，获得了 " << user->getReward() << " 个奖励" << endl;
+//    }
+}
+
+double Pool::getDowntvl()
+{
+    return m_downtvl;
+}
---
+++ v3/Pool/Pool.h
@@ -0,0 +1,65 @@
+//
+// Created by Vc on 1/16/24.
+//
+
+#ifndef DOUBLER_POOL_H
+#define DOUBLER_POOL_H
+
+#include <list>
+
+#include "../User/User.h"
+
+namespace FEE{
+    const double ECO_FEE = 0.2;
+    const double WITHDRAW_FEE = 0.02;
+    const double BORROW_FEE = 0.02;
+}
+
+class User;
+
+class Pool {
+public:
+    Pool(double units, double fall_ratio, double profit_lock, double profit_rtatio, double re_share_ratio, double max_reward,
+         double  spot_price, double pool_dbr, double pool_layer, bool pool_status,
+         const std::vector<double> &layer_input, const std::vector<double> layer_price);
+    void ca_layer_dbr(); //计算每层dbr
+
+    void addUser(User* user);
+    void distribute();
+    void ca_pool_esreward();
+    double getDowntvl();
+
+public:
+    const double m_units;
+    const double m_pool_dbr;
+    double m_profit_price = 0;
+    double m_avg_price = 0;
+    double m_pool_tvl = 0;
+    double m_es_reward = 0;
+    double m_pool_input_count = 0;
+
+private:
+    const double m_fall_ratio;
+    const double m_profit_lock;
+    const double m_profit_rtatio;
+    const double m_re_share_ratio;
+    const double m_max_reward;
+
+    const double m_spot_price;
+    const int m_pool_layer;
+    const bool m_pool_status;
+
+    double m_downtvl;
+    double m_uptvl;
+
+private:
+    std::vector<double> pool_layer_input;
+    std::vector<double> pool_layer_price;
+    std::list<std::pair<double, double>> pool_layer_tvl;
+
+    std::vector<User*> m_users;
+    std::vector<double> layer_dbr;
+};
+
+
+#endif //DOUBLER_POOL_H
